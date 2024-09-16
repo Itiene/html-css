8689812dc8a88f97849970017d961571bdaa32a7
Itiene <guilherme.itiene@gmail.com>
2024-09-16 19:27:55 +0100

    Initial commit

---
+++ exercicios/ex016/might delete later/aula.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+
+    printf("Digite algo:");
+
+    scanf("%d",&n);
+
+    printf("O numero é: %d ", n);
+
+    system("pause");   
+    return 0;
+
+}
